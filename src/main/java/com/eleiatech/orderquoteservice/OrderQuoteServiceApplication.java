e7455d14ed25ccefc2a3e023f8c05d18b5e4b9ae
eleiatech <71985257+eleiatech@users.noreply.github.com>
2020-12-11 22:53:25 +0300

    Initial commit

--- src/main/java/com/eleiatech/orderquoteservice/OrderQuoteServiceApplication.java
+++ src/main/java/com/eleiatech/orderquoteservice/OrderQuoteServiceApplication.java
@@ -2,8 +2,12 @@ package com.eleiatech.orderquoteservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableJpaAuditing
 @SpringBootApplication
 public class OrderQuoteServiceApplication {
